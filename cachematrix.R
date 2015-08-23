5c501a3c379c5449b52b4371f2eaa299796160a8
Juvoni Beckford <message@juvoni.com>
2015-08-23 16:29:23 -0400

    Added function body

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# @name makeCacheMatrix
+# @description Create a Matrix Object to cache it's Inverse
+# @param  x default Matrix
+# @return List of public functions: setters and getters
+#/
 makeCacheMatrix <- function(x = matrix()) {
-
+       	_inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                _inverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) _inverse <<- inverse
+        getInverse <- function() inverse
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# @name cacheSolve
+# @description Get the inverse of a Matrix
+# @param  x
+# @return Matrix Inverse Object
+#/
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        _inverse <- x$getInverse()
+        if (!is.null(_inverse)) {
+                message('Retreiving cached data...')
+                return(_inverse)
+        }
+        _matrix <- x$get()
+        _inverse <- solve(_matrix, ...)
+        x$setInverse(_inverse)
+        _inverse
 }
